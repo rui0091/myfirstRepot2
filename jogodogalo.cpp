921225f6598eac1140465c5f391356ba5683851c
rui0091 <diogo.rui2008@gmail.com>
2025-10-27 11:55:56 -0100

    Update jogodogalo.cpp

--- jogodogalo.cpp
+++ jogodogalo.cpp
@@ -6,34 +6,36 @@ struct jogoGalo{
     char a;
     bool b = false;
 };
-jogoGalo jogo[3][3];
+
+jogoGalo jogo[3][3];  //array criado por minha pessoa
 
 void mostrar(){
     cout << "Jogo do galo" << endl;
     cout << "   1   2   3" << endl;
     cout << "1 [" << jogo[0][0].a << "] [" << jogo[0][1].a << "] [" << jogo[0][2].a << "]" << endl;
-    cout << "2 [" << jogo[1][0].a << "] [" << jogo[1][1].a << "] [" << jogo[1][2].a << "]" << endl;
+    cout << "2 [" << jogo[1][0].a << "] [" << jogo[1][1].a << "] [" << jogo[1][2].a << "]" << endl;    //mostrar a tabela
     cout << "3 [" << jogo[2][0].a << "] [" << jogo[2][1].a << "] [" << jogo[2][2].a << "]" << endl;
         }
-int main(){
+
+        int main(){
     int coluna;
     int linha;
     char jogador;
 
-mostrar();
+mostrar();  //MOSTRA A FUNÇÃO
     cout <<"Digite qual queres ser X ou O" << endl;
 cin >> jogador;
-for(int i = 0; i<=8; i++){
+for(int i = 0; i<=8; i++){       //mostrar no terminal qual coluna e sua respetiva linha
     cout <<"Digite a coluna que quer marcar" << endl;
 cin >> coluna;
     cout <<"Digite a linha que quer marcar" << endl;
 cin >> linha;
 
-jogo[coluna-1][linha-1].a = jogador;
-jogo[coluna-1][linha-1].b = true;
+jogo[coluna-1][linha-1].a = jogador; //vai pegar no array vai meter a jogada
+jogo[coluna-1][linha-1].b = true;    // esse irá dizer que está ocupado
 
 mostrar ();
-if(jogador == 'X'){
+if(jogador == 'X'){   //trocando com o X e o 0 
     jogador = 'O';
 
 }else 
